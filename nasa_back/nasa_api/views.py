bdacb00c65db91767384ad28dff6508efea969a0
chariot <88636741+chariot27@users.noreply.github.com>
2024-10-05 14:31:55 -0300

    'uppando'

--- nasa_back/nasa_api/views.py
+++ nasa_back/nasa_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializer import CometsSerializer
 from .models import Comets
 
 # Create your views here.
@@ -8,4 +9,11 @@ class CometView(APIView):
     def get(self, request):
         comets = Comets.objects.all()
         return Response({'comets': comets.values()})
+    def post(self, request):
+        serializer = CometsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
 
